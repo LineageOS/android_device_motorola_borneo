5369ae341f6f78225363795139d125cdc36a07b6
Michael Bestas <mkbestas@lineageos.org>
2023-05-10 23:05:41 +0300

    borneo: Decommonize audio_policy_volumes.xml
    Change-Id: I92e1c1a95b6986c90038f04507de27e848b63b15

--- device.mk
+++ device.mk
@@ -10,6 +10,7 @@ $(call inherit-product, device/motorola/sm6225-common/bengal.mk)
 # Audio - Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
