d6ad1a1faf9ee45491bbd65745c1202f8c021c79
Michael Bestas <mkbestas@lineageos.org>
2023-05-10 18:38:34 +0300

    borneo: Update odm sku manifests
    Change-Id: I7c056eadea13751fffa64444f5eb9b52bac3a3c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,17 +16,9 @@ TARGET_BOOTLOADER_BOARD_NAME := borneo
 TARGET_SCREEN_DENSITY := 280
 
 # HIDL
-ODM_MANIFEST_SKUS += b bc d dc dn dnc f fc n nc
+ODM_MANIFEST_SKUS += b f
 ODM_MANIFEST_B_FILES := $(DEVICE_PATH)/sku/manifest_b.xml
-ODM_MANIFEST_BC_FILES := $(DEVICE_PATH)/sku/manifest_bc.xml
-ODM_MANIFEST_D_FILES := $(DEVICE_PATH)/sku/manifest_d.xml
-ODM_MANIFEST_DC_FILES := $(DEVICE_PATH)/sku/manifest_dc.xml
-ODM_MANIFEST_DN_FILES := $(DEVICE_PATH)/sku/manifest_dn.xml
-ODM_MANIFEST_DNC_FILES := $(DEVICE_PATH)/sku/manifest_dnc.xml
 ODM_MANIFEST_F_FILES := $(DEVICE_PATH)/sku/manifest_f.xml
-ODM_MANIFEST_FC_FILES := $(DEVICE_PATH)/sku/manifest_fc.xml
-ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/sku/manifest_n.xml
-ODM_MANIFEST_NC_FILES := $(DEVICE_PATH)/sku/manifest_nc.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/ext_config/borneo-default.config
