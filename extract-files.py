446581d415f928c3e86a28ec4d8db9a9a81a85ca
Deivid21 <david.parra.ignacio@gmail.com>
2025-10-15 01:11:27 -0300

    borneo: Patch vidhance components with libui-v34
    * EIS was broken, which caused hard crashes when switching
      to video mode in camera.
    
    Change-Id: If7d3b4417208fbb51466992aece7ec9b82a1cd41

--- extract-files.py
+++ extract-files.py
@@ -39,6 +39,11 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
     'vendor/lib64/libvidhance.so': blob_fixup()
         .add_needed('libcomparetf2_shim.so'),
+    (
+        'vendor/lib64/camera/components/com.qti.node.dewarp.so',
+        'vendor/lib64/camera/components/com.vidhance.node.processing.so',
+    ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
 }  # fmt: skip
 
 extract_fns: extract_fns_user_type = {
