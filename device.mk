721384d43d16592e9aa4f183eb8ee7cf95395abf
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    borneo: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
 # LiveDisplay
-$(call soong_config_set,livedisplay_sysfs,enable_ab,true)
+$(call soong_config_set_bool,livedisplay_sysfs,enable_ab,true)
 
 # Overlay
 PRODUCT_PACKAGES += \
